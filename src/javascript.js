677970df8124d7a0bb94b4b1e68b09729bedf49d
ibrahim wael <ibrahimwael809@gmail.com>
2024-10-03 23:45:26 +0300

    Add files via upload

---
+++ src/javascript.js
@@ -0,0 +1,16 @@
+// var scrollGap = 1
+// var prevScrollPos = 0
+// var currentFrame = 1
+// var frameCount = 500
+
+// window.addEventListener('scroll', function() {
+//     var image = document.getElementById('scrollImage');
+//     // if (this.window.scrollY > prevScrollPos + scrollGap){
+//         currentFrame = Math.ceil(window.scrollY / this.window.innerHeight * frameCount);
+//         if (currentFrame == 0) currentFrame = 1
+//         image.src = ' ../../assets/rendering/'+ currentFrame.toString().padStart(4, '0') + '.png'
+
+//         prevScrollPos = window.scrollY; 
+//     // }
+
+// });
